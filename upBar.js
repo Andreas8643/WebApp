6a7f4872d114fc11c19e1bfc7188a62be5cadb3e
Andreas8643 <146427052+Andreas8643@users.noreply.github.com>
2024-09-25 16:28:15 +0300

    Create upBar.js

---
+++ upBar.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('.link-other-website');
+
+    links.forEach((link, index) => {
+        if (index < links.length - 1) {
+            const separator = document.createElement('span');
+            separator.classList.add('separator');
+            
+            link.parentNode.insertAdjacentElement('afterend', separator);
+        }
+    });
+});
