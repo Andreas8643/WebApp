b788e81a8ae626c93687e9491fe000382e55bf2e
Andreas8643 <146427052+Andreas8643@users.noreply.github.com>
2024-11-24 23:52:05 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,7 @@ import LoginView from '@/views/LoginView.vue';// Login page
 import SignUp from '@/views/SignUp.vue';      // SignUp page
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView,
-    beforeEnter: (to, from, next) => {
-      store.dispatch('fetchPosts').then(() => {
-        next(); // Jätka lehe laadimist alles pärast postituste laadimist
-      });
-    },
-  },
+  { path: '/', name: 'Home', component: HomeView },
   { path: '/addPost', name: 'AddPost', component: AddPost },
   { path: '/login', name: 'Login', component: LoginView },
   {path: '/signup', name: 'SignUp', component: SignUp}
