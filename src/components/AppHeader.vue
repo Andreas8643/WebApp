8d00c02be2e43285ddec6b99a6c73e5215178d05
marten0703 <150028831+marten0703@users.noreply.github.com>
2024-12-14 16:38:19 +0200

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,29 +13,39 @@
     </a>
     <!-- Use v-if to toggle the dropdown visibility -->
     <div v-if="isDropdownVisible" id="dropdown-menu" class="dropdown-content">
-      <p>John Doe</p>
-      <p>johndoe@ut.ee</p>
+      <p v-if="userEmail">{{ userEmail }}</p> <!-- Show user email -->
       <!-- Use a router-link to navigate to the login page -->
-      <router-link to="/login" @click="closeDropdown">Log out</router-link>
+      <router-link to="/login" @click="logout">Log out</router-link>
     </div>
   </header>
 </template>
 
 <script>
+import { getLoggedInUser, logoutUser } from "@/utils/auth";
+
 export default {
   name: "AppHeader",
   data() {
     return {
-      isDropdownVisible: false, // initial state of dropdown visibility
+      isDropdownVisible: false, // Controls dropdown visibility
+      userEmail: null, // Stores the logged-in user's email
     };
   },
   methods: {
     toggleDropdown() {
       this.isDropdownVisible = !this.isDropdownVisible;
     },
-    closeDropdown() {
-      this.isDropdownVisible = false; // Close dropdown when navigating
+    logout() {
+      logoutUser(); // Clear token and log out user
+      this.userEmail = null; // Reset email
+      this.$router.push("/login"); // Redirect to login page
     },
+  },
+  mounted() {
+    const loggedInUser = getLoggedInUser(); // Fetch user data from JWT
+    if (loggedInUser) {
+      this.userEmail = loggedInUser.email; // Display user's email
+    }
   },
 };
 </script>
